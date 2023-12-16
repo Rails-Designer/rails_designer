19cb3572f2d40b99dbf35562b68e2ad539230ec8
eelco <git@eelcoj.com>
2023-12-16 18:47:05 +0000

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Rails Designer
+
+Check out [Rails Designer](https://railsdesigner.com/) on how to get the actual Rails Designer gem.
---
+++ lib/rails_designer.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "rails_designer/version"
+
+module RailsDesigner
+  class Error < StandardError; end
+  # Your code goes here...
+end
